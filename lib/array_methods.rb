81661f6ef568700e5b0671b282be3a2a2fd3c26e
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-22 02:21:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,10 @@
 def find_element_index(array, value_to_find)
   counter = 0
   while counter > array.length
-    if array.include?(value_to_find)
+    if array[counter] == value_to_find
     p counter
     counter += 1
+  end
   end
 end
 
