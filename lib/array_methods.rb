73ef4d5521caa9c60d8bbfaaab76ff751c1d0cdb
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-22 02:32:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,12 +2,11 @@ def find_element_index(array, value_to_find)
   counter = 0
   while counter > array.length
     if array[counter] == value_to_find
-  else
-
+      return counter
   end
   counter += 1
   end
-  p counter
+  #p counter
 end
 
 def find_max_value(array)
