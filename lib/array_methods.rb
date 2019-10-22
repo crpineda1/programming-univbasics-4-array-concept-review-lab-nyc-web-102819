81b72ef11242f545dbec10c0715cbc1cc473145e
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-22 02:49:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,7 @@ end
 
 def find_max_value(array)
   counter = 0
-  max_value = 0
+  max_value = array[counter]
   while counter < array.length
     if max_value < array[counter]
       max_value = array[counter]
@@ -22,7 +22,7 @@ end
 
 def find_min_value(array)
   counter = 0
-  min_value = 0
+  min_value = array[counter]
   while counter < array.length
     if min_value > array[counter]
       min_value = array[counter]
